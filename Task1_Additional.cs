641873c81431b354aa181efb5675d31b06d77cde
OleksandrProvolovskyi <47982795+OleksandrProvolovskyi@users.noreply.github.com>
2019-03-01 10:54:45 +0200

    Add files via upload

---
+++ Task1_Additional.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClass : Initialization
+    {
+        [Test]
+        public void Test1()
+        {
+            driver.FindElement(By.XPath("//a[@id='uhfLogo']")).Click();
+        }
+    }
+    
+}
---
+++ Task1_Additional2.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    public class Initialization
+    {
+        public IWebDriver driver;
+        [SetUp]
+        public void BeforeTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Navigate().GoToUrl("https://www.skype.com/uk/");
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Close();
+        }
+    }
+}
